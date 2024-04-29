e20f2e9a386f1bd36d682a44f39ef26246cfc4ff
laurirasanen <lauri@rasanen.dev>
2024-04-29 22:34:58 +0300

    update link script

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
+
+set -euo pipefail
 
 TF="$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf"
 
 mkdir -pv "$TF/scripts/vscripts/"
-ln -s "$(pwd)/domc" "$TF/scripts/vscripts/domc"
+ln -s "$(pwd)/scripts/vscripts/domc" "$TF/scripts/vscripts/domc"
+
